9ed579c64f8835d9664ea329e17c7275889d83ac
Oscar Arvidsson <oscar.arvidsson@protonmail.com>
2026-06-25 00:20:06 +0200

    new easier to follow design, works

--- assistant.py
+++ assistant.py
@@ -4,37 +4,18 @@ from enum import Enum
 from pathlib import Path
 from ollama import Client
 
-COLOR_PROMPT = "\033[31m"
-COLOR_USER_TEXT = "\033[36m"
+COLOR_PROMPT = "\033[1;31m"
+COLOR_USER_TEXT = "\033[1;36m"
 COLOR_RESET = "\033[0m"
 
+outputTag = "[MODE:OUTPUT]"
+reasoningTag = "[MODE:REASONING]"
+toolTag = "[MODE:TOOL]"
 historyPath = Path.cwd() / "history.txt"
 sysPromptPath = Path.cwd() / "sysprompt.txt"
 longTermMemoryPath = Path.cwd() / "longtermmemory.txt"
 configPath = Path.cwd() / "config.txt"
 ollamaClient = Client()
-model = None
-
-history = []
-if (historyPath.exists()):
-    history = json.loads(historyPath.read_text())
-
-if (longTermMemoryPath.exists()):
-    history.insert(0, {"role": "system", "content": longTermMemoryPath.read_text()})
-
-if (sysPromptPath.exists()):
-    history.insert(0, {"role": "system", "content": sysPromptPath.read_text()})
-
-def globalExceptionHandler(excType, value, traceback):
-    if issubclass(excType, KeyboardInterrupt):
-        print(COLOR_RESET + "\n[exit]", end="")
-        for i, msg in enumerate(history):
-            if msg["role"] != "system":
-                historyPath.write_text(json.dumps(history[i:]))
-                break;
-        sys.exit()
-
-sys.excepthook = globalExceptionHandler
 
 def cfgFindModel(modelName):
     for mdl in ollamaClient.list().models:
@@ -42,52 +23,120 @@ def cfgFindModel(modelName):
             return mdl["model"]
     return None
 
-if (configPath.exists()):
-    for line in configPath.read_text().splitlines():
-        if line.startswith("model="):
-            model = cfgFindModel(line.split("=", 1)[1].strip())
-            if model is None:
-                model = ollamaClient.list().models[0]["model"]
-                print("[config error] model \"" f"{line.split('=', 1)[1].strip()}\" " "not found")
-else:
-    configPath.write_text(f"model={model}\n")
+class Mode(Enum):
+    OUTPUT = 0
+    REASONING = 1
+    TOOL = 2
+    POSSIBLE = 3
+    NOSWITCH = 4
 
-outputTag = "{\"mode\":\"output\"}"
-reasoningTag = "{\"mode\":\"reasoning\"}"
+def tryModeSwitch(buf):
+    if buf == outputTag[:len(buf)] or buf == reasoningTag[:len(buf)] or buf == toolTag[:len(buf)]:
+        if buf == outputTag:
+            return Mode.OUTPUT
+        elif buf == reasoningTag:
+            return Mode.REASONING
+        elif buf == toolTag:
+            return Mode.TOOL
+        else:
+            return Mode.POSSIBLE
+    return Mode.NOSWITCH
 
-print("[model]", model)
-while True:
-    history.append({"role": "user", "content": input(COLOR_PROMPT + ">> " + COLOR_USER_TEXT)})
-    print(COLOR_RESET, end="");
-    stream = ollamaClient.chat(
-        model=model,
-        messages=history,
-        stream=True)
+def callTool(obj):
+    try:
+        if obj["tool"] == "remember":
+            #return toolRemember(obj["args"])
+            return None
+        elif obj["tool"] == "forget":
+            #return toolForget(obj["args"])
+            return None
+        else:
+            return None
+    except KeyError:
+        return None
 
-    buf = ""
-    outputMode = False
-    history.append({"role": "assistant", "content": ""})
-    for chunk in stream:
-        history[-1]["content"] += chunk.message.content
-        for c in chunk.message.content:
-            buf += c
-            if outputMode:
-                if buf == reasoningTag[:len(buf)]:
-                    if len(buf) == len(reasoningTag):
-                        outputMode = False
-                        buf = ""
-                else:
-                    print(buf, end="")
+def tryParseToolCall(toolHistory, buf):
+    try:
+        obj = json.loads(buf)
+        result = callTool(obj)
+        if result == None:
+            toolHistory.append({"role": "tool", "content": "error: tool not found"})
+        else:
+            toolHistory.append({"role": "tool", "content": result})
+    except json.JSONDecodeError:
+        pass
+
+def main():
+    history = []
+    if (historyPath.exists()):
+        history = json.loads(historyPath.read_text())
+
+    if (longTermMemoryPath.exists()):
+        history.insert(0, {"role": "system", "content": longTermMemoryPath.read_text()})
+
+    if (sysPromptPath.exists()):
+        history.insert(0, {"role": "system", "content": sysPromptPath.read_text()})
+
+    def keyboardInterruptHandler(excType, value, traceback):
+        if issubclass(excType, KeyboardInterrupt):
+            print(COLOR_RESET + "\n[exit]", end="")
+            for i, msg in enumerate(history):
+                if msg["role"] != "system":
+                    history.append({"role": "user", "content": "keyboard interrupt"})
+                    historyPath.write_text(json.dumps(history[i:]))
+                    break;
+        else:
+            sys.__excepthook__(excType, value,traceback)
+        sys.exit()
+
+    sys.excepthook = keyboardInterruptHandler
+    model = None
+    if (configPath.exists()):
+        for line in configPath.read_text().splitlines():
+            if line.startswith("model="):
+                model = cfgFindModel(line.split("=", 1)[1].strip())
+                if model is None:
+                    model = ollamaClient.list().models[0]["model"]
+                    print("[config] error: model \"" f"{line.split('=', 1)[1].strip()}\" " "not found")
+    else:
+        configPath.write_text(f"model={model}\n")
+
+    print("[model]", model)
+    while True:
+        history.append({"role": "user", "content": input(COLOR_PROMPT + ">> " + COLOR_USER_TEXT)})
+        print(COLOR_RESET, end="");
+        stream = ollamaClient.chat(
+            model=model,
+            messages=history,
+            stream=True)
+
+        mode = Mode.REASONING
+        history.append({"role": "assistant", "content": ""})
+        buf = ""
+        toolHistory = []
+        for chunk in stream:
+            history[-1]["content"] += chunk.message.content
+            for c in chunk.message.content:
+                buf += c
+                ret = tryModeSwitch(buf);
+                if ret == Mode.OUTPUT or ret == Mode.REASONING or ret == Mode.TOOL:
+                    if mode == Mode.TOOL:
+                        toolHistory.append({"role": "tool", "content": "error: invalid json"})
+                    mode = ret
                     buf = ""
-            else:
-                if buf == outputTag[:len(buf)]:
-                    if len(buf) == len(outputTag):
-                        outputMode = True
+                elif ret == Mode.NOSWITCH:
+                    if mode == Mode.OUTPUT:
+                        print(buf, end="", flush=True)
                         buf = ""
-                #elif c == "}":
-                    #try to parse a json object
-                    #see if it is a tool call
-                    #call the tool call if it is a tool call
-                else:
-                    buf = ""
-    print()
+                    elif mode == Mode.REASONING:
+                        buf = ""
+
+                if mode == Mode.TOOL:
+                    tryParseToolCall(toolHistory, buf)
+
+        history.extend(toolHistory)
+        toolHistory.clear()
+        print()
+
+if __name__ == "__main__":
+    main()
